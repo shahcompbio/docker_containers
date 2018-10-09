a52d821b24fc45054612864d7094e8fdef4955a8
Mike Yue <myue@bccrc.ca>
2018-10-09 16:14:02 -0700

    Added script subdir

---
+++ Script/build_script.py
@@ -0,0 +1,28 @@
+import subprocess
+from git import Git, Repo
+import os
+
+if __name__ == "__main__":
+	local_repo = Repo(os.getcwd())
+	branch = local_repo.active_branch
+	print branch.name
+	VERSION_NOT_FOUND = True
+	while VERSION_NOT_FOUND:
+		try:
+			subprocess.call(["ls", "VERSION"])
+			with open("VERSION", "r") as version_file:
+				data = version_file.read()
+				print("Current VERSION is: " + data)
+			VERSION_NOT_FOUND = False
+		except OSError:
+			subprocess.call(["cd", ".."])
+	'''subprocess.call(["ls", "-l"])
+	with open("~/" + branch.name + "/" + "VERSION", "r") as version_file:
+		data = version_file.read()
+	print("Current VERSION is: " + data)
+	new_version = input("Please enter the updated VERSION number: ")
+	with open("VERSION", "w") as version_file:
+		version_file.seek(0)
+		version_file.write(str(new_version))
+		version_file.truncate()
+	print ("New version is %s" % new_version)'''
