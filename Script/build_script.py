d5d00595281275e14d60b46433dc3a1a81151af8
Mike Yue <myue@bccrc.ca>
2018-10-11 12:03:08 -0700

    Error handling

--- Script/build_script.py
+++ Script/build_script.py
@@ -2,18 +2,33 @@ import subprocess
 from git import Git, Repo
 import os
 
+def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]
 #Tag convertion should go: FOLDER_NAME-Version_Number
 #Example: bwa-0.0.1
 if __name__ == "__main__":
 
+	#list_of_all_dirs = []
 	#Test Registry. Change to Actual Registry when code is in production
 	registry_url = "singlecellcontainers.azurecr.io"
-	print os.getcwd()
+	list_of_all_dirs = get_immediate_subdirectories(os.getcwd())
+	print list_of_all_dirs
 	repo = Repo(os.getcwd())
 	tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-	print (tags[-1])
-	folder_name = str(tags[-1]).split('-')[0].lower()
-	new_version = str(tags[-1]).split('-')[1]
+
+	#Checks for tag formatting
+	if(len(str(tags[-1]).split('-')) == 2):
+		folder_name = str(tags[-1]).split('-')[0].lower()
+		new_version = str(tags[-1]).split('-')[1]
+	else:
+		print ("Incorrectly formatted tag. Exiting script...")
+		quit()
+
+	#Checks for directory existence corresponding to tag prefix
+	if(folder_name not in list_of_all_dirs):
+		print ("Tag does not correspond to any folder. Exiting script...")
+		quit()
 
 	try:
 		username = os.environ['CLIENT_ID']
@@ -21,25 +36,11 @@ if __name__ == "__main__":
 	except (KeyError) as e:
 		print (e)
 
-	docker_login_cmd = "docker login -u " + username + " -p " + password
-	docker_build_cmd = "cd " + folder_name + " ; docker build -t " + folder_name + " ." 
-	#docker_tag_cmd = 
-	#docker_push_cmd = 
+	#TODO Error Handling
 	try:
-		subprocess.call(["docker" , "login", "singlecellcontainers.azurecr.io", "-u", username, "--password", password])
+		subprocess.call(["docker" , "login", registry_url, "-u", username, "--password", password])
 		subprocess.call("{}; {}".format("cd " + folder_name, "docker build -t " + folder_name + " ."), shell=True)
-		print ("Finished Build")
-
-		#############################################################################################
-		#         																					#
-		# Might be a better way to do this. Don't really feel comfortable using hardcoded numbers   #
-		#																							#
-		#############################################################################################
-		#build_id = output.splitlines()[-2].split()[-1] #Build ID is the last string in the second last line of terminal output after running docker build
-
-		subprocess.call(["docker", "tag", folder_name, "singlecellcontainers.azurecr.io/scp/" + folder_name + ":" + new_version])
-		print("Finished Tagging")
-		
-		subprocess.call(["docker", "push", "singlecellcontainers.azurecr.io/scp/" + folder_name + ":" + new_version])
+		subprocess.call(["docker", "tag", folder_name, registry_url + "/scp/" + folder_name + ":" + new_version])
+		subprocess.call(["docker", "push", registry_url + "/scp/" + folder_name + ":" + new_version])
 	except OSError as e:
 		print (e)
